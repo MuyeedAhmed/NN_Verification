b1d8c00589776a78a5b51ad9a44413fd8309f823
Muyeed Ahmed <akib100095@gmail.com>
2025-06-14 14:43:49 -0400

    RAB: add VGG in the mix for SVHN

--- CNN_RetrainAfterBorder/CNNetworks.py
+++ CNN_RetrainAfterBorder/CNNetworks.py
@@ -237,3 +237,34 @@ class NIN(nn.Module):
         x = torch.relu(x)
         x = self.classifier(x)
         return x
+
+class VGG(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),  # 16x16
+
+            nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),  # 8x8
+
+            nn.Conv2d(128, 256, 3, padding=1), nn.ReLU(), nn.BatchNorm2d(256),
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+
+        self.flatten = nn.Flatten()
+        self.fc_hidden = nn.Linear(256, 128)
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(128, num_classes)
+    
+    def forward(self, x, extract_fc_input=False):
+        x = self.features(x)
+        x = self.flatten(x)
+        if extract_fc_input:
+            return x.clone().detach(), None
+        x = self.fc_hidden(x)
+        x = self.relu(x)
+        x = self.classifier(x)
+        return x
