e2f8b8d06bc100f026860348daef617bbdf33b55
Muyeed Ahmed <akib100095@gmail.com>
2025-06-26 23:03:33 -0400

    Gen stats

--- CNN_RetrainAfterGurobi/Figure&Stats.py
+++ CNN_RetrainAfterGurobi/Figure&Stats.py
@@ -3,55 +3,78 @@ import matplotlib.pyplot as plt
 import os
 
 def summarize_statistics(df, dataset_name, run_id):
+    if "Train" not in df['Phase'].values: # For RAF
+        gurobi_df = df[df['Phase'] == 'GurobiEdit']
+        S3_train_acc = gurobi_df['Train_acc'].iloc[-1]
+        S3_train_loss = gurobi_df['Train_loss'].iloc[-1]
+        S3_test_loss = df[df['Phase'] == 'GurobiEdit_Test']['Train_loss'].iloc[0]
+        S3_test_acc = df[df['Phase'] == 'GurobiEdit_Test']['Train_acc'].iloc[0]
+        S3_Start_train_acc = df[df['Phase'] == 'Gurobi_Complete_Eval_Train']['Train_acc'].iloc[0]
+        S3_Start_train_loss = df[df['Phase'] == 'Gurobi_Complete_Eval_Train']['Train_loss'].iloc[0]
+        S3_Start_test_acc = df[df['Phase'] == 'Gurobi_Complete_Eval_Val']['Train_acc'].iloc[0]
+        S3_Start_test_loss = df[df['Phase'] == 'Gurobi_Complete_Eval_Val']['Train_loss'].iloc[0]
+        summary_df = pd.DataFrame({
+            'Dataset': [dataset_name],
+            'Run ID': [run_id],
+            'S3_Start_Train_Acc': [S3_Start_train_acc],
+            'S3_Start_Train_Loss': [S3_Start_train_loss],
+            'S3_Start_Test_Acc': [S3_Start_test_acc],
+            'S3_Start_Test_Loss': [S3_Start_test_loss],
+            'S3_Train_Acc': [S3_train_acc],
+            'S3_Train_Loss': [S3_train_loss],
+            'S3_Test_Acc': [S3_test_acc],
+            'S3_Test_Loss': [S3_test_loss]
+        })
+        return summary_df
     train_df = df[df['Phase'] == 'Train']
-    resume_df = df[df['Phase'] == 'Resume']
+    resume_df = df[df['Phase'] == 'ResumeTrain']
     gurobi_df = df[df['Phase'] == 'GurobiEdit']
-    gurobi_eval_df = df[df['Phase'] == 'Gurobi_Complete_Eval']
-
-    train_end_epoch = train_df['Epoch'].max()
     
     S1_train_acc = train_df['Train_acc'].iloc[-1]
     S1_train_loss = train_df['Train_loss'].iloc[-1]
-    S1_val_acc = train_df['Val_acc'].iloc[-1]
-    S1_val_loss = train_df['Val_loss'].iloc[-1]
+    S1_test_acc = df[df['Phase'] == 'Train_Test']['Train_acc'].iloc[0]
+    S1_test_loss = df[df['Phase'] == 'Train_Test']['Train_loss'].iloc[0]
     
     if not resume_df.empty:
         S2_train_acc = resume_df['Train_acc'].iloc[-1]
         S2_train_loss = resume_df['Train_loss'].iloc[-1]
-        S2_val_acc = resume_df['Val_acc'].iloc[-1]
-        S2_val_loss = resume_df['Val_loss'].iloc[-1]
+        S2_test_acc = df[df['Phase'] == 'ResumeTrain_Test']['Train_acc'].iloc[0]
+        S2_test_loss = df[df['Phase'] == 'ResumeTrain_Test']['Train_loss'].iloc[0]
+
+    
+    S3_Start_train_acc = df[df['Phase'] == 'Gurobi_Complete_Eval_Train']['Train_acc'].iloc[0]
+    S3_Start_train_loss = df[df['Phase'] == 'Gurobi_Complete_Eval_Train']['Train_loss'].iloc[0]
+    S3_Start_test_acc = df[df['Phase'] == 'Gurobi_Complete_Eval_Val']['Train_acc'].iloc[0]
+    S3_Start_test_loss = df[df['Phase'] == 'Gurobi_Complete_Eval_Val']['Train_loss'].iloc[0]
+
 
     S3_train_acc = gurobi_df['Train_acc'].iloc[-1]
     S3_train_loss = gurobi_df['Train_loss'].iloc[-1]
-    S3_val_acc = gurobi_df['Val_acc'].iloc[-1]
-    S3_val_loss = gurobi_df['Val_loss'].iloc[-1]
-
-    gurobi_eval_acc_train = gurobi_eval_df['Train_acc'].iloc[0]
-    gurobi_eval_loss_train = gurobi_eval_df['Train_loss'].iloc[0]
-    gurobi_eval_acc_val = gurobi_eval_df['Val_acc'].iloc[0]
-    gurobi_eval_loss_val = gurobi_eval_df['Val_loss'].iloc[0]
-
-    # Create a summary DataFrame
+    S3_test_loss = df[df['Phase'] == 'GurobiEdit_Test']['Train_loss'].iloc[0]
+    S3_test_acc = df[df['Phase'] == 'GurobiEdit_Test']['Train_acc'].iloc[0]
+    
+    
     summary_df = pd.DataFrame({
         'Dataset': [dataset_name],
         'Run ID': [run_id],
         'S1_Train_Acc': [S1_train_acc],
         'S1_Train_Loss': [S1_train_loss],
-        'S1_Val_Acc': [S1_val_acc],
-        'S1_Val_Loss': [S1_val_loss],
-        'S2_Train_Acc': [S2_train_acc] if 'S2_train_acc' in locals() else [None],
-        'S2_Train_Loss': [S2_train_loss] if 'S2_train_loss' in locals() else [None],
-        'S2_Val_Acc': [S2_val_acc] if 'S2_val_acc' in locals() else [None],
-        'S2_Val_Loss': [S2_val_loss] if 'S2_val_loss' in locals() else [None],
+        'S1_Test_Acc': [S1_test_acc],
+        'S1_Test_Loss': [S1_test_loss],
+        'S2_Train_Acc': [S2_train_acc] if 'S2_train_acc' in locals() else [S1_train_acc],
+        'S2_Train_Loss': [S2_train_loss] if 'S2_train_loss' in locals() else [S1_train_loss],
+        'S2_Test_Acc': [S2_test_acc] if 'S2_test_acc' in locals() else [S1_test_acc],
+        'S2_Test_Loss': [S2_test_loss] if 'S2_test_loss' in locals() else [S1_test_loss],
+        'S3_Start_Train_Acc': [S3_Start_train_acc],
+        'S3_Start_Train_Loss': [S3_Start_train_loss],
+        'S3_Start_Test_Acc': [S3_Start_test_acc],
+        'S3_Start_Test_Loss': [S3_Start_test_loss],
         'S3_Train_Acc': [S3_train_acc],
         'S3_Train_Loss': [S3_train_loss],
-        'S3_Val_Acc': [S3_val_acc],
-        'S3_Val_Loss': [S3_val_loss],
-        'Gurobi_Eval_Train_Acc': [gurobi_eval_acc_train],
-        'Gurobi_Eval_Train_Loss': [gurobi_eval_loss_train],
-        'Gurobi_Eval_Val_Acc': [gurobi_eval_acc_val],
-        'Gurobi_Eval_Val_Loss': [gurobi_eval_loss_val]
+        'S3_Test_Acc': [S3_test_acc],
+        'S3_Test_Loss': [S3_test_loss]
     })
+
     return summary_df
 
 
@@ -119,8 +142,8 @@ def save_training_history(history, file_path):
 
 
 def main():
-    Test = "Test"
-    folder_path = "Stats_"+ Test
+    Test = "RAF_CrossVal_All"
+    folder_path = "Stats/"+ Test
     fig_folder = "Figures/" + Test
     Stats_folder = "Stats"
     if not os.path.exists(fig_folder):
@@ -135,35 +158,53 @@ def main():
         for run_id in history['Run'].unique():
             run_history = history[history['Run'] == run_id]
             if not run_history.empty:
-                plot_training_history(run_history, dataset_name, run_id, fig_folder)
-                summary_df = summarize_statistics(run_history, dataset_name, run_id)
-                dataset_stats_summary = pd.concat([dataset_stats_summary, summary_df], ignore_index=True)
+                try:
+                # plot_training_history(run_history, dataset_name, run_id, fig_folder)
+                    summary_df = summarize_statistics(run_history, dataset_name, run_id)
+                    dataset_stats_summary = pd.concat([dataset_stats_summary, summary_df], ignore_index=True)
+                except Exception as e:
+                    print(f"Error processing {dataset_name} Run {run_id}: {e}")
         if not dataset_stats_summary.empty:
-            avg_summary = pd.DataFrame({
-                'Dataset': [dataset_name],
-                'Run ID': "Average",
-                'S1_Train_Acc': dataset_stats_summary["S1_Train_Acc"].mean(),
-                'S1_Train_Loss': dataset_stats_summary["S1_Train_Loss"].mean(),
-                'S1_Val_Acc': dataset_stats_summary["S1_Val_Acc"].mean(),
-                'S1_Val_Loss': dataset_stats_summary["S1_Val_Loss"].mean(),
-                'S2_Train_Acc': dataset_stats_summary["S2_Train_Acc"].mean(),
-                'S2_Train_Loss': dataset_stats_summary["S2_Train_Loss"].mean(),
-                'S2_Val_Acc': dataset_stats_summary["S2_Val_Acc"].mean(),
-                'S2_Val_Loss': dataset_stats_summary["S2_Val_Loss"].mean(),
-                'S3_Train_Acc': dataset_stats_summary["S3_Train_Acc"].mean(),
-                'S3_Train_Loss': dataset_stats_summary["S3_Train_Loss"].mean(),
-                'S3_Val_Acc': dataset_stats_summary["S3_Val_Acc"].mean(),
-                'S3_Val_Loss': dataset_stats_summary["S3_Val_Loss"].mean(),
-                'Gurobi_Eval_Train_Acc': dataset_stats_summary["Gurobi_Eval_Train_Acc"].mean(),
-                'Gurobi_Eval_Train_Loss': dataset_stats_summary["Gurobi_Eval_Train_Loss"].mean(),
-                'Gurobi_Eval_Val_Acc': dataset_stats_summary["Gurobi_Eval_Val_Acc"].mean(),
-                'Gurobi_Eval_Val_Loss': dataset_stats_summary["Gurobi_Eval_Val_Loss"].mean()
-            })
+            if Test == "RAF_CrossVal_All":
+                avg_summary = pd.DataFrame({
+                    'Dataset': [dataset_name],
+                    'Run ID': "Average",
+                    'S3_Start_Train_Acc': dataset_stats_summary["S3_Start_Train_Acc"].mean(),
+                    'S3_Start_Train_Loss': dataset_stats_summary["S3_Start_Train_Loss"].mean(),
+                    'S3_Start_Test_Acc': dataset_stats_summary["S3_Start_Test_Acc"].mean(),
+                    'S3_Start_Test_Loss': dataset_stats_summary["S3_Start_Test_Loss"].mean(),
+                    'S3_Train_Acc': dataset_stats_summary["S3_Train_Acc"].mean(),
+                    'S3_Train_Loss': dataset_stats_summary["S3_Train_Loss"].mean(),
+                    'S3_Test_Acc': dataset_stats_summary["S3_Test_Acc"].mean(),
+                    'S3_Test_Loss': dataset_stats_summary["S3_Test_Loss"].mean()
+                })
+            else:
+                avg_summary = pd.DataFrame({
+                    'Dataset': [dataset_name],
+                    'Run ID': "Average",
+                    'S1_Train_Acc': dataset_stats_summary["S1_Train_Acc"].mean(),
+                    'S1_Train_Loss': dataset_stats_summary["S1_Train_Loss"].mean(),
+                    'S1_Test_Acc': dataset_stats_summary["S1_Test_Acc"].mean(),
+                    'S1_Test_Loss': dataset_stats_summary["S1_Test_Loss"].mean(),
+                    'S2_Train_Acc': dataset_stats_summary["S2_Train_Acc"].mean() if 'S2_Train_Acc' in dataset_stats_summary else None,
+                    'S2_Train_Loss': dataset_stats_summary["S2_Train_Loss"].mean() if 'S2_Train_Loss' in dataset_stats_summary else None,
+                    'S2_Test_Acc': dataset_stats_summary["S2_Test_Acc"].mean() if 'S2_Test_Acc' in dataset_stats_summary else None,
+                    'S2_Test_Loss': dataset_stats_summary["S2_Test_Loss"].mean() if 'S2_Test_Loss' in dataset_stats_summary else None,
+                    'S3_Start_Train_Acc': dataset_stats_summary["S3_Start_Train_Acc"].mean(),
+                    'S3_Start_Train_Loss': dataset_stats_summary["S3_Start_Train_Loss"].mean(),
+                    'S3_Start_Test_Acc': dataset_stats_summary["S3_Start_Test_Acc"].mean(),
+                    'S3_Start_Test_Loss': dataset_stats_summary["S3_Start_Test_Loss"].mean(),
+                    'S3_Train_Acc': dataset_stats_summary["S3_Train_Acc"].mean(),
+                    'S3_Train_Loss': dataset_stats_summary["S3_Train_Loss"].mean(),
+                    'S3_Test_Acc': dataset_stats_summary["S3_Test_Acc"].mean(),
+                    'S3_Test_Loss': dataset_stats_summary["S3_Test_Loss"].mean()
+                })
+            
             dataset_stats_summary = pd.concat([dataset_stats_summary, avg_summary], ignore_index=True)
         Stats_summary = pd.concat([Stats_summary, dataset_stats_summary], ignore_index=True)
 
     # Save the summary statistics to a CSV file
-    Stats_summary.to_csv(f'{Stats_folder}/Stats_summary_{Test}.csv', index=False)                
+    Stats_summary.to_excel(f'{Stats_folder}/Summary_{Test}.xlsx', index=False)              
         
     
 if __name__ == "__main__":
