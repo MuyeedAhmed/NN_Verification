c7de7f71ec4942790addb14da3d2b8cab79da15e
Muyeed Ahmed <akib100095@gmail.com>
2026-01-12 17:58:12 -0500

    CIFAR10 add RandomCrop and RandomHorizontalFlip

--- Ensemble/Main.py
+++ Ensemble/Main.py
@@ -71,7 +71,7 @@ def GetDataset(dataset_name, root_dir='./data', device=None, classCount=None):
         test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
     elif dataset_name == "CIFAR10":
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
+        transform = transforms.Compose([transforms.RandomCrop(32, padding=4),transforms.RandomHorizontalFlip(),transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
         train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
         test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
         all_classes = train_dataset.classes
