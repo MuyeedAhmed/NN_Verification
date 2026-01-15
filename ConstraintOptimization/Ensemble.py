c6c72eaae38cb1dc6e4d195aeddafe9e101cbec4
Muyeed Ahmed <akib100095@gmail.com>
2026-01-15 15:41:41 -0500

    -

--- ConstraintOptimization/Ensemble.py
+++ ConstraintOptimization/Ensemble.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
     initEpoch = 300
-    G_epoch = 0
+    G_epoch = 50
     
     misclassification_counts = [1, 5, 10, 20]
     top_k = 10
