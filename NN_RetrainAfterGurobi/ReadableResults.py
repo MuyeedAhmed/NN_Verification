745eacb0e47f6e8658a1ce74107232667e22b907
Muyeed Ahmed <akib100095@gmail.com>
2026-01-24 17:11:20 -0500

    NN: Update readtable.py

--- NN_RetrainAfterGurobi/ReadableResults.py
+++ NN_RetrainAfterGurobi/ReadableResults.py
@@ -17,9 +17,7 @@ def Summarize(df):
         # print(grouped)
         init_test_acc = grouped.loc[grouped["Method"] == "Train", "test_acc_mean"].iloc[0]
         # print(f"Dataset: {dataset}, Train Test Acc: {init_test_acc}")
-        grouped["Delta_test_acc_vs_init"] = (
-            grouped["test_acc_mean"] - init_test_acc
-        )
+        grouped["Delta_test_acc_vs_init"] = (grouped["test_acc_mean"] - init_test_acc) * 100.0
         # df_delta = grouped[grouped["Method"] != "Train"]
         df_raf = grouped[grouped["Method"].str.contains("RAF", na=False)]
         # print(df_raf)
